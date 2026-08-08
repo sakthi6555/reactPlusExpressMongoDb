55625e56a4880033e7a6d3dafe008c3661bcd3bc
Sakthivel <sakthi.65555@gmail.com>
2026-08-08 20:10:59 +0530

    Initial React UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
 import UserForm from './components/UserForm'
